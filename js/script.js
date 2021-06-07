7a22072a37f83a6d4846403b8f3a44f7b2a515a4
Boris <filippovbor@gmail.com>
2021-06-07 13:38:29 +0300

    Домашняя работа 4 (долг) и 5

--- js/script.js
+++ js/script.js
@@ -1,48 +1,70 @@
-// С помощью цикла while вывести все простые числа в промежутке от 0 до 100.
+"use strict"
+"use strict";
 
-let n = 100;
+/**
+ * @property {HTMLElement} gameContainerEl Контейнер игры (DOM элемент).
+ */
+const chess = {
+    gameContainerEl: document.getElementById('game'),
 
-continuationLabel: for (let i = 2; i <= n; i++) {
+    /**
+     * Метод отображения карты (игрового поля).
+     */
+    renderMap() {
+        // Строки, которые есть на поле.
+        const rows = [0, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+        // Колонки, которые есть на поле.
+        const cols = [0, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 0];
 
-    for (let j = 2; j < i; j++) {
-        if (i % j == 0) continue continuationLabel;
-    }
+        // Пробегаемся по каждой строке.
+        for (let row = 0; row < rows.length; row++) {
+            // Создаем элемент строки.
+            const tr = document.createElement('tr');
+            // Добавляем строку в контейнер игры.
+            this.gameContainerEl.appendChild(tr);
 
-    console.log(i);
-}
+            // Пробегаемся по каждой колонке.
+            for (let col = 0; col < cols.length; col++) {
+                // Создаем элемент ячейки.
+                const td = document.createElement('td');
+                // Добавляем ячейку в строку.
+                tr.appendChild(td);
 
-// 2. С этого урока начинаем работать с функционалом интернет-магазина. Предположим, есть сущность корзины. Нужно реализовать функционал подсчета стоимости корзины в зависимости от находящихся в ней товаров.
-// 3. Товары в корзине хранятся в массиве. Задачи:
-// a) Организовать такой массив для хранения товаров в корзине;
-// b) Организовать функцию countBasketPrice, которая будет считать стоимость корзины./ 
+                // Если либо строка, либо колонка равна 0, значит это не игровое поле.
+                if (rows[row] === 0 && cols[col] !== 0) {
+                    // Если это верхнее или нижнее поля, отображаем какие колонки есть, 0 не выводим.
+                    td.innerHTML = cols[col];
+                } else if (cols[col] === 0 && rows[row] !== 0) {
+                    // Если это левое или правое поля, отображаем цифры игрового поля, 0 не выводим.
+                    td.innerHTML = rows[row].toString();
+                }
 
-const basket = [
-    ['Ноутбук', 45000, 1],
-    ['Мышька', 1000, 2],
-];
+                // Если ячейка черная - красим ее.
+                if (this.isCellIsBlack(row, col)) {
+                    td.style.backgroundColor = 'grey';
+                }
+            }
+        }
+    },
 
-// Вариант через for
-// function countBasketPrice(basket) {
-//   let totalPrice = 0;
-//   for (let i = 0; i < basket.length; i++) {
-//     totalPrice += basket[i][1] * basket[i][2];
-//   }
-//   return totalPrice;
-// }
-// Вариант через forEach
-// function countBasketPrice(basket) {
-//   let totalPrice = 0;
-//   basket.forEach((cartItem) => {
-//     totalPrice += cartItem[1] * cartItem[2];
-//   });
-//   return totalPrice;
-// }
-// Самый оптимальный вариант через reduce
-function countBasketPrice(basket) {
-    // return basket.reduce((totalPrice, cartItem) => totalPrice + cartItem[1] * cartItem[2], 0);
-    return basket.reduce(function (totalPrice, cartItem) {
-        return totalPrice + cartItem[1] * cartItem[2];
-    }, 0);
-}
+    /**
+     * Определяет является ли ячейка черной.
+     * @param {int} rowNum Номер в строке.
+     * @param {int} colNum Номер в колонке.
+     * @returns {boolean} true, если ячейка должна быть черной, иначе false.
+     */
+    isCellIsBlack(rowNum, colNum) {
+        // Если ячейка боковая (не игровое поле), их красить не нужно.
+        if (rowNum === 0 || colNum === 0 || rowNum === 9 || colNum === 9) {
+            return false;
+        }
 
-console.log(countBasketPrice(basket));
+        // Определяем по четности/нечетности строки и колонки.
+        return (rowNum % 2 === 1 && colNum % 2 === 0) || (rowNum % 2 === 0 && colNum % 2 === 1);
+        // Либо можно сделать проще.
+        // return (rowNum + colNum) % 2 === 1;
+    },
+};
+
+// Запускаем метод отображения карты.
+chess.renderMap();
